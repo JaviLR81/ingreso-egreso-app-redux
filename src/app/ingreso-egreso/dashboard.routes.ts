1d9a3bc978371cd19c7a6d2cbd19f0da0080f7ec
Javier Lozano <javierlzrd@hotmail.com>
2022-08-16 10:35:08 -0500

    initial commit

---
+++ src/app/ingreso-egreso/dashboard.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from "@angular/router";
+import { DetalleComponent } from "./detalle/detalle.component";
+import { EstadisticaComponent } from "./estadistica/estadistica.component";
+import { IngresoEgresoComponent } from "./ingreso-egreso.component";
+
+
+export const dashboardRoutes: Routes = [
+  {
+    path: '',
+    component: EstadisticaComponent
+  },
+  {
+    path: 'ingreso-egreso',
+    component: IngresoEgresoComponent
+  },
+  {
+    path: 'detalle',
+    component: DetalleComponent
+  },
+];
